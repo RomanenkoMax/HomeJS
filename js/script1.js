5c2f3cf8dd54fde414a226fad49ebbf4778a0001
RomanenkoMax <romanenkomax.ua@gmail.com>
2019-03-18 17:17:23 +0200

    add advansed js homework 4

--- js/script1.js
+++ js/script1.js
@@ -15,20 +15,60 @@ function createTable() {
         table.appendChild(tr);
     }
     table.style.borderCollapse = 'collapse';
-    body.insertBefore(table, document.getElementsByTagName('script')[0]);
+    $('body > script').before(table);
 }
 
 createTable();
 
 let myTable = document.getElementsByTagName('table')[0];
 myTable.addEventListener('click', function (event) {
-    event.target.classList.toggle('active');
+    $(event.target).css('background-color', function (index, oldValue) {
+
+        if (oldValue == 'rgb(255, 255, 255)') {
+            return 'black';
+        } else {
+            return 'white';
+        }
+    });
     event.stopPropagation();
 });
 
 let myBody = document.body;
 myBody.addEventListener('click', function (event) {
-    if (event.currentTarget !== 'td'){
-        myTable.classList.toggle('active');
-    };
+    $('table *').css('background-color', function (index, oldVal) {
+        if (oldVal == 'rgb(255, 255, 255)') {
+            return 'black';
+        } else {
+            return 'white';
+        }
+    });
 });
+
+// $('body').on('click', 'table', function (e) {
+//
+//    $(e.target).css('background-color', function (index, oldValue) {
+//
+//        if (oldValue == 'rgb(255, 255, 255)') {
+//            return 'black';
+//        } else {
+//            return 'white';
+//        }
+//    });
+//    e.stopPropagation();
+// });
+
+// $('body').on('click', 'body', function (e) {
+//     $('table').css('background-color', function (index, oldVal) {
+//         if (oldValue == 'rgb(255, 255, 255)') {
+//             return 'black';
+//         } else {
+//             return 'white';
+//         }
+//     })
+// });
+
+// let myTable = document.getElementsByTagName('table')[0];
+// myTable.addEventListener('click', function (event) {
+//     event.target.classList.toggle('active');
+//     event.stopPropagation();
+// });
